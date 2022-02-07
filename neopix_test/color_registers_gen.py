9d6b171e57f9393e6e55923e47b8fc782806b734
allincmu <allin@andrew.cmu.edu>
2022-02-06 21:38:15 -0500

    code to generate system verilog for color registers

--- neopix_test/color_registers_gen.py
+++ neopix_test/color_registers_gen.py
@@ -1,5 +1,5 @@
-times = 17
-for i in range(times):
-  print(f"""Register #(8) r_r{i}(.D(green), .clear, .en(en[{i}]), .clock, .Q(lldata[{times*24-i*24-1}:{times*24-i*24-8}]));
-Register #(8) r_g{i}(.D(  red), .clear, .en(en[{i}]), .clock, .Q(lldata[{times*24-i*24-9}:{times*24-i*24-8*2}]));
-Register #(8) r_b{i}(.D( blue), .clear, .en(en[{i}]), .clock, .Q(lldata[{times*24-i*24-8*2-1}:{times*24-i*24-8*3}]));""")
+NUM_NPX = 17
+for i in range(NUM_NPX):
+  print(f"""Register #(8) r_r{i}(.D(green), .clear, .en(en[{i}]), .clock, .Q(lldata[{NUM_NPX*24-i*24-1}:{NUM_NPX*24-i*24-8}]));
+Register #(8) r_g{i}(.D(  red), .clear, .en(en[{i}]), .clock, .Q(lldata[{NUM_NPX*24-i*24-9}:{NUM_NPX*24-i*24-8*2}]));
+Register #(8) r_b{i}(.D( blue), .clear, .en(en[{i}]), .clock, .Q(lldata[{NUM_NPX*24-i*24-8*2-1}:{NUM_NPX*24-i*24-8*3}]));""")
